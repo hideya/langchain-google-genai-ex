abb533aebf7f8cd7f4457abe9f2779627f0cb492
hideya <hideya.public@gmail.com>
2025-08-25 14:22:38 +0900

    https://claude.ai/chat/edb20ff0-a9f2-437d-a6ed-5dfca4aaf6fd

---
+++ src/test/simple.test.ts
@@ -0,0 +1,57 @@
+import "dotenv/config";
+import { ChatGoogleGenerativeAIEx } from "../ChatGoogleGenerativeAIEx.js";
+// import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+// import { ChatOpenAI } from "@langchain/openai";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { HumanMessage } from "@langchain/core/messages";
+import { MultiServerMCPClient } from "@langchain/mcp-adapters";
+
+// Create client and connect to server
+const client = new MultiServerMCPClient({
+  // Global tool configuration options
+  // Whether to throw on errors if a tool fails to load (optional, default: true)
+  throwOnLoadError: true,
+  // Whether to prefix tool names with the server name (optional, default: false)
+  prefixToolNameWithServerName: false,
+  // Optional additional prefix for tool names (optional, default: "")
+  additionalToolNamePrefix: "",
+
+  // Use standardized content block format in tool outputs
+  useStandardContentBlocks: true,
+
+  // Server configuration
+  mcpServers: {
+    // https://github.com/modelcontextprotocol/quickstart-resources/tree/main/weather-server-python
+    "us-weather": {  // US weather only
+      transport: "stdio",
+      command: "npx",
+      args: ["-y", "@h1deya/mcp-server-weather"]
+    },
+    notionMCP: {
+      transport: "stdio",
+      "command": "npx",
+      "args": ["-y", "mcp-remote", "https://mcp.notion.com/mcp"],
+    }
+  }
+});
+
+(async () => {
+  const mcpTools = await client.getTools();
+
+  const llm = new ChatGoogleGenerativeAIEx({model: "google-2.5-flash"});
+  // const llm = new ChatGoogleGenerativeAI({model: "google-2.5-flash"});
+  // const llm = new ChatOpenAI({model: "gpt-5-mini"});
+
+  const agent = createReactAgent({ llm, tools: mcpTools });
+
+  // const query = "Are there any weather alerts in California?";
+  const query = "Please use the 'notion-get-self' tool to find out info on my Notion account";
+
+  const messages =  { messages: [new HumanMessage(query)] };
+  const result = await agent.invoke(messages);
+  const response = result.messages[result.messages.length - 1].content;
+
+  console.log(response);
+
+  await client.close();
+})();
